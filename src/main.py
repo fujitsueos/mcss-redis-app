370e0e2d57679c19569bb74c58a788c317deed98
lukasz-kuch <lukas.walbrzych@gmail.com>
2020-11-16 14:43:03 +0100

    add app.yaml

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from flask import Flask
 import redis
 
 app = Flask(__name__)
+app_name = os.environ.get('APP_NAME', 'dev')
 
 redis_host = os.environ.get('REDISHOST', 'localhost')
 redis_port = int(os.environ.get('REDISPORT', 7001))
@@ -12,7 +13,7 @@ redis_client = redis.StrictRedis(host=redis_host, port=redis_port)
 
 @app.route('/')
 def index():
-    return {'message': 'Hello, there!'}
+    return {'appName': app_name}
 
 @app.route('/api/redis')
 def api_redis_get():
